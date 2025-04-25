3c01be4902b69111fe790c9146a3c05595af2f1a
sujit7890 <96191474+sujit7890@users.noreply.github.com>
2025-04-25 11:45:33 +0530

    Sujit initial commit

---
+++ demo/src/test/java/com/example/demo/TestSample.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestSample {
+    @Test
+    public void testExample() {
+        assertEquals(1 + 1, 2);
+    }
+}
